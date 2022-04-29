930ea7574cee01e5ff82068379654a9daf4b102d
Roelfie <58694687+roelfie@users.noreply.github.com>
2022-04-29 23:34:58 +0200

    Manual steps after installing apps.

---
+++ setup_apps_manual.zsh
@@ -0,0 +1,6 @@
+#!/usr/bin/env zsh
+
+echo "\n<<< Manually register / configure apps where necessary >>>\n"
+
+echo "TODO instructions per installed app what to do (like upload license key)."
+vared -p "Press [Enter] to continue. " -c REPLY
